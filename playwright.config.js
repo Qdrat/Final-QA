6e7ed61579201432395acaa0fd4c4763c435ab02
Qdrat <alfredhager0420@gmail.com>
2026-07-29 16:05:04 +0300

    add tg_bot_

--- playwright.config.js
+++ playwright.config.js
@@ -60,8 +60,8 @@ export default defineConfig({
             use: { ...devices['Pixel 5'] },
         },
         {
-          name: 'Mobile Safari',
-          use: { ...devices['iPhone 12'] },
+            name: 'Mobile Safari',
+            use: { ...devices['iPhone 12'] },
         },
 
         /* Test against branded browsers. */
